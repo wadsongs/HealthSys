dc359bcfa6e05da89c61ecee877d993ecb39db1e
Wadson Gurgel <ad_gurgel@hotmail.com>
2026-03-28 03:06:46 -0300

    Subindo arquivos iniciais

---
+++ Backend/pacientes/src/main/java/com/br/unifor/pacientes/PacientesApplication.java
@@ -0,0 +1,13 @@
+package com.br.unifor.pacientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacientesApplication.class, args);
+	}
+
+}
---
+++ Backend/pacientes/src/test/java/com/br/unifor/pacientes/PacientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.unifor.pacientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
