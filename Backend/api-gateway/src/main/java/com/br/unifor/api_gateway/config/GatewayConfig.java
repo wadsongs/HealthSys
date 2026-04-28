d2502041fa40be8630ed4a7a3f9bc3ebfd08c2aa
Wadson <57421936+wadsongs@users.noreply.github.com>
2026-04-28 13:52:40 -0300

    feat:gateway

---
+++ Backend/api-gateway/src/main/java/com/br/unifor/api_gateway/config/GatewayConfig.java
@@ -0,0 +1,4 @@
+package com.br.unifor.api_gateway.config;
+
+public class GatewayConfig {
+}
---
+++ Backend/api-gateway/src/main/java/com/br/unifor/api_gateway/filter/JwtAuthGatewayFilter.java
@@ -0,0 +1,84 @@
+package com.br.unifor.api_gateway.filter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.crypto.SecretKey;
+
+@Slf4j
+@Component
+public class JwtAuthGatewayFilter extends AbstractGatewayFilterFactory<JwtAuthGatewayFilter.Config> {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public JwtAuthGatewayFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            String authHeader = exchange.getRequest()
+                    .getHeaders()
+                    .getFirst(HttpHeaders.AUTHORIZATION);
+
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                return semAutorizacao(exchange, "Token não fornecido");
+            }
+
+            try {
+                String token = authHeader.substring(7);
+                Claims claims = extrairClaims(token);
+
+                ServerWebExchange exchangeModificado = exchange.mutate()
+                        .request(exchange.getRequest().mutate()
+                                .header("X-User-Id", claims.get("id").toString())
+                                .header("X-User-Perfil", claims.get("perfil").toString())
+                                .header("X-User-Email", claims.getSubject())
+                                .build())
+                        .build();
+
+                return chain.filter(exchangeModificado);
+            } catch (Exception e) {
+
+                log.error("Token inválido: {}", e.getMessage());
+                return semAutorizacao(exchange, "Token inválido ou expirado");
+            }
+        };
+    }
+
+    private Claims extrairClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getChave())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+    }
+
+    private SecretKey getChave() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    private Mono<Void> semAutorizacao (ServerWebExchange exchange, String mensagem) {
+        log.warn("Acesso negado: {}" ,mensagem);
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
+    }
+
+    public static class Config {
+
+    }
+
+}
